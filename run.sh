4ec63f7806a00a403b2571acf19134b4324d22ed
Kevin Muller <kevin@passbolt.com>
2017-05-13 17:40:42 +0530

    MUX-18 improved container and binary paths

--- run.sh
+++ run.sh
@@ -11,21 +11,26 @@ else
     KEEP_ALIVE=1
 fi
 
-# Copy the mailvelope_selenium key inside the mailvelope directory.
-cp /var/www/mailvelope_selenium/config/key.pem /var/www/mailvelope/.travis/key.pem
-
-# Build maivelope extension for chrome and firefox.
-cd /var/www/mailvelope && grunt build
-# Build for ff.
-grunt dist-ff
-# Build for chrome.
-./.travis/crxmake.sh ./build/chrome /var/www/mailvelope/.travis/key.pem
-
-# create symlinks.
-ln -s -f /var/www/mailvelope/dist/mailvelope.chrome.crx /var/www/mailvelope_selenium/data/extensions/
-ln -s -f /var/www/mailvelope/dist/mailvelope.xpi /var/www/mailvelope_selenium/data/extensions/
-
 if [ "$RUN_SELENIUM" == "1" ]; then
+    # Copy the mailvelope_selenium key inside the mailvelope directory.
+    cp /var/www/mailvelope_selenium/config/key.pem /var/www/mailvelope/.travis/key.pem
+
+    echo "build plugin"
+    NODE_VERSION=`node -v`
+    GRUNT="/root/.nvm/versions/node/$NODE_VERSION/bin/grunt"
+
+    # Build maivelope extension for chrome and firefox.
+    cd /var/www/mailvelope
+    $GRUNT
+    # Build for ff.
+    $GRUNT dist-ff
+    # Build for chrome.
+    ./.travis/crxmake.sh ./build/chrome /var/www/mailvelope/.travis/key.pem
+
+    # create symlinks.
+    ln -s -f /var/www/mailvelope/dist/mailvelope.chrome.crx /var/www/mailvelope_selenium/data/extensions/
+    ln -s -f /var/www/mailvelope/dist/mailvelope.xpi /var/www/mailvelope_selenium/data/extensions/
+
     cd /var/www/mailvelope_selenium
     #./node_modules/.bin/wdio ./config/firefox.conf.js
     ./node_modules/.bin/wdio ./config/chrome.conf.js
